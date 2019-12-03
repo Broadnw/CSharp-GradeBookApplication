ecdc5c5a6be35e885d979b6ab88d6c719cee1ee7
William Broadnax <allnax@yahoo.com>
2019-12-02 17:09:43 -0800

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -29,5 +29,15 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
     }
 }
